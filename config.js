d1d4358a66332eecc576af1ff3e861ddfcbf4a24
jrcis08 <66540500+jrcis08@users.noreply.github.com>
2026-08-31 01:26:07 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BASE_DOMAIN: 'https://imgmask.onrender.com',
+    BASE_DOMAIN: 'https://maru-leaks.onrender.com',
     PATHS: {
         abstract: '/ff8e82d46d81d435e50f66fa4a702d63.jpg'
     }
