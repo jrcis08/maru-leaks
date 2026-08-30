6ee2e38f27b77a7a3abb2954db75f11066d41af8
jrcis08 <66540500+jrcis08@users.noreply.github.com>
2026-08-31 00:54:22 +1200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BASE_DOMAIN: 'https://imgrask.anvender.com',
+    BASE_DOMAIN: 'https://imgmask.onrender.com',
     PATHS: {
         abstract: '/ff8e82d46d81d435e50f66fa4a702d63.jpg'
     }
